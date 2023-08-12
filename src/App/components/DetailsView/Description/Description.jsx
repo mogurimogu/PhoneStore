2d2761ce943311924b095396cc93328832d77b40
Luis Javier <ljanobenitez@gmail.com>
2023-08-12 12:54:21 +0200

    Añadido condiciones para la descripción

--- src/App/components/DetailsView/Description/Description.jsx
+++ src/App/components/DetailsView/Description/Description.jsx
@@ -19,7 +19,7 @@ function Description({ productDetails }) {
   } = productDetails;
 
   const listTitleClasses = "font-semibold";
-  const SubListClasses= "list-disc list-inside pl-5"
+  const SubListClasses = "list-disc list-inside pl-5";
 
   return (
     <>
@@ -27,55 +27,73 @@ function Description({ productDetails }) {
         <h1 className="text-2xl font-semibold text-white">
           {brand} {model}
         </h1>
-        <span className="text-5xl font-semibold text-amber-500">
-          {price}€
-        </span>
+        <span className="text-5xl font-semibold text-amber-500">{price}€</span>
       </div>
       <ul className="p-3">
-        <li>
-          <span className={listTitleClasses}>CPU: </span>
-          {cpu}
-        </li>
-        <li>
-          <span className={listTitleClasses}>RAM: </span>
-          {ram}
-        </li>
-        <li>
-          <span className={listTitleClasses}>Sistema Operativo: </span>
-          {os}
-        </li>
-        <li>
-          <span className={listTitleClasses}>Pantalla:</span>
-          <ul className={SubListClasses}>
-            <li>{displayType}</li>
-            <li>{displayResolution}</li>
-            <li>{displaySize}</li>
-          </ul>
-        </li>
-        <li>
-          <span className={listTitleClasses}>Betería:</span> {battery}
-        </li>
-        <li>
-          <span className={listTitleClasses}>Cámaras:</span>
-          <ul className={SubListClasses}>
-            <li>
-              <span className={listTitleClasses}>Principal:</span>{" "}
-              {primaryCamera}
-            </li>
-            <li>
-              <span className={listTitleClasses}>Secundaria:</span>{" "}
-              {secondaryCmera}
-            </li>
-          </ul>
-        </li>
-        <li>
-          <span className={listTitleClasses}>Dimensiones: </span>
-          {dimentions}
-        </li>
-        <li>
-          <span className={listTitleClasses}>Peso: </span>
-          {weight}g
-        </li>
+        {cpu && (
+          <li>
+            <span className={listTitleClasses}>CPU: </span>
+            {cpu}
+          </li>
+        )}
+        {ram && (
+          <li>
+            <span className={listTitleClasses}>RAM: </span>
+            {ram}
+          </li>
+        )}
+        {os && (
+          <li>
+            <span className={listTitleClasses}>Sistema Operativo: </span>
+            {os}
+          </li>
+        )}
+        {(displayResolution || displaySize || displayType) && (
+          <li>
+            <span className={listTitleClasses}>Pantalla:</span>
+            <ul className={SubListClasses}>
+              {displayType && <li>{displayType}</li>}
+              {displayResolution && <li>{displayResolution}</li>}
+              {displaySize && <li>{displaySize}</li>}
+            </ul>
+          </li>
+        )}
+        {battery && (
+          <li>
+            <span className={listTitleClasses}>Betería:</span> {battery}
+          </li>
+        )}
+        {(primaryCamera || secondaryCmera) && (
+          <li>
+            <span className={listTitleClasses}>Cámaras:</span>
+            <ul className={SubListClasses}>
+              {primaryCamera && (
+                <li>
+                  <span className={listTitleClasses}>Principal:</span>{" "}
+                  {primaryCamera}
+                </li>
+              )}
+              {secondaryCmera && (
+                <li>
+                  <span className={listTitleClasses}>Secundaria:</span>{" "}
+                  {secondaryCmera}
+                </li>
+              )}
+            </ul>
+          </li>
+        )}
+        {dimentions && (
+          <li>
+            <span className={listTitleClasses}>Dimensiones: </span>
+            {dimentions}
+          </li>
+        )}
+        {weight && (
+          <li>
+            <span className={listTitleClasses}>Peso: </span>
+            {weight}g
+          </li>
+        )}
       </ul>
     </>
   );
